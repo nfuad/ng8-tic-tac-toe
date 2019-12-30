d8c0523d09a1f0b2618646423fc0a20cebc69391
Nafis Fuad <fuadbd14@gmail.com>
2019-12-30 18:38:37 +0600

    Add github corner

---
+++ src/app/github-corner/github-corner.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-github-corner",
+  templateUrl: "./github-corner.component.html"
+})
+export class GithubCornerComponent {}
