4af1b80d4e3b64b7ac46818c775e293f235c0750
Nafis Fuad <fuadbd14@gmail.com>
2019-12-30 21:49:08 +0600

    Remove nebular stuff

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,16 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-square',
+  selector: "app-square",
   template: `
-    <button nbButton *ngIf="!value">{{ value }}</button>
-    <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
-    <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
+    <button *ngIf="!value">{{ value }}</button>
+    <button *ngIf="value == 'X'">
+      {{ value }}
+    </button>
+    <button *ngIf="value == 'O'">
+      {{ value }}
+    </button>
   `,
-  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
+  styles: ["button { width: 100%; height: 100%; font-size: 5em !important; }"]
 })
-export class SquareComponent  {
-
-  @Input() value: 'X' | 'O';
-
+export class SquareComponent {
+  @Input() value: "X" | "O";
 }
