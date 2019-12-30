6e6466326ad9def352fbb7bd82b81ce5e01a1784
Nafis Fuad <fuadbd14@gmail.com>
2019-12-30 23:18:52 +0600

    Update game logic and styles

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-board',
-  templateUrl: './board.component.html',
-  styleUrls: ['./board.component.scss']
+  selector: "app-board",
+  templateUrl: "./board.component.html",
+  styleUrls: ["./board.component.scss"]
 })
 export class BoardComponent implements OnInit {
   squares: string[];
   xIsNext: boolean;
   winner: string;
+  tapSound = new Audio("../assets/sound.mp3");
 
   constructor() {}
 
@@ -23,13 +24,16 @@ export class BoardComponent implements OnInit {
   }
 
   get player() {
-    return this.xIsNext ? 'X' : 'O';
+    return this.xIsNext ? "X" : "O";
   }
 
   makeMove(idx: number) {
-    if (!this.squares[idx]) {
+    if (!this.squares[idx] && !this.winner) {
       this.squares.splice(idx, 1, this.player);
       this.xIsNext = !this.xIsNext;
+
+      // play tap sound
+      this.tapSound.play();
     }
 
     this.winner = this.calculateWinner();
